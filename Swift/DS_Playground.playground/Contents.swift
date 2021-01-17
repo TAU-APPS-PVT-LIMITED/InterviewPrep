10dc84614754970a6fbad750e00d135619c7d2cb
Gaurav Gupta <gauravgupta@mobilityquarks.com>
2021-01-17 17:57:07 +0530

    added sample code for linked list

--- Swift/DS_Playground.playground/Contents.swift
+++ Swift/DS_Playground.playground/Contents.swift
@@ -1,2 +1,29 @@
 import Cocoa
 
+protocol LinkListNode {
+    var next: LinkListNode? {get set}
+    var value: String {get set}
+}
+
+struct Node:LinkListNode {
+    var next: LinkListNode?
+    var value: String
+
+    mutating func insert(next:inout Node) {
+        next.next = self.next
+        self.next = next
+    }
+
+}
+
+var last = Node(next: nil, value: "lastNode")
+var start = Node(next: last, value: "start")
+
+print("start next node value is \(start.next?.value ?? "null value")")
+
+var node = Node(next: nil, value: "next node")
+start.insert(next: &node)
+
+print("start next node value is \(start.next?.value ?? "null value")")
+print("start next next node value is \(start.next?.next?.value ?? "null value")")
+
