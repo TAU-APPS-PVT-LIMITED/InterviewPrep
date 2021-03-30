22e2ed52c7c87414aa74bdff6b6579ecdf3573c2
Gaurav Gupta <gauravgupta@mobilityquarks.com>
2021-03-30 16:30:05 +0530

    fixed some vector insertion

--- CPP/linkedlistOps/testLL/lab7.cpp
+++ CPP/linkedlistOps/testLL/lab7.cpp
@@ -6,16 +6,16 @@ using namespace std;
 
 int main () {
     LinkedList myLL,newLL;
-    vector<char> suspectDNA = {'C','C','G','A','C','G','A','G','G','T','C','T','A','A','T','T','T','G','C','A','G','G','T','C','C','T','C','T','A','G'};
+    vector<char> suspectDNA = {'C','C','T','A','A','T','T','T','G','C','A','G','A','G'};
     vector<char> evidence = {'A','A','T','T','T','G','C','A','G'};
     LinkedList llsuspectDNA, llevidenceDNA;
-    for(vector<char>::iterator i = suspectDNA.begin(); i != suspectDNA.end() ; i++){
+    for(vector<char>::reverse_iterator i = suspectDNA.rbegin(); i != suspectDNA.rend() ; i++){
         llsuspectDNA.InsertFront(*i);
     }
     llsuspectDNA.Display();
 
     for(vector<char>::iterator i = evidence.begin(); i != evidence.end() ; i++){
-        llevidenceDNA.InsertFront(*i);
+        llevidenceDNA.InsertEnd(*i);
     }
     llevidenceDNA.Display();
 
@@ -30,7 +30,8 @@ int main () {
 //    for(int i = 0; i < 4; i++){
 //        newLL.InsertFront(i);
 //    }
-    bool isSuspectMatched = llevidenceDNA.compareDNA(llsuspectDNA);
+    //bool isSuspectMatched = llevidenceDNA.compareDNA(llsuspectDNA);
+    bool isSuspectMatched = llsuspectDNA.compareDNA(llevidenceDNA);
     cout<<"Result of existence check of newLL in myLL: "<<(isSuspectMatched == 1 ? "Exists":"Not Exists")<<endl;
 //    myLL.Display();
 //    newLL.Display();
