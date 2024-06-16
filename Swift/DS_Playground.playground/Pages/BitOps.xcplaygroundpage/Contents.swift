e77f2f7382e87e8c55f1ac678f3dc3893f435829
Gaurav Gupta <1276964+ggthedev@users.noreply.github.com>
2024-06-16 06:54:50 +0530

    added code for bitlength calculation program

--- Swift/DS_Playground.playground/Pages/BitOps.xcplaygroundpage/Contents.swift
+++ Swift/DS_Playground.playground/Pages/BitOps.xcplaygroundpage/Contents.swift
@@ -8,4 +8,18 @@ var num1 = 9
 num1 = num1 >> 1
 print("Num1 : \(num1)")
 
+/*++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ Find the bit length of a number
+ +++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+func bitLen(num:Int) -> Int {
+  var bitcounter = 0
+  while (( 1 << bitcounter) <= num){
+    bitcounter += 1
+  }
+  return bitcounter
+}
+
+print("Bit length of 14 is : \(bitLen(num: 14))")
+print("Bit length of 14 is : \(bitLen(num: 44))")
 //: [Next](@next)
