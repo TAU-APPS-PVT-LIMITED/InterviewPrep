7e522d1b954ace2a1b01546b08b639e3afd4e602
Gaurav Gupta <1276964+ggthedev@users.noreply.github.com>
2024-06-21 01:49:17 +0530

    added file capturing basic string operations as well as examples of the course: educative.io

--- python/source/main.py
+++ python/source/main.py
@@ -1,19 +1,22 @@
 import operators_example as op
 import bit_manipulations as bimap
 import course_problems as cp
+from string_mysteries import string_chars_ops
+
 # operators_example is a file and is present in the same folder as main.py.
 # sources folder 
 
 
 def main():
-    bimap.bit_manipulations()
-    op.operators_example()
-    cp.ordered_pairs()
+    # bimap.bit_manipulations()
+    # op.operators_example()
+    # cp.ordered_pairs()
     # cp.square_from_asteriks()
     # cp.asteriks_right_triangle()
     # cp.asteriks_hollow_square()
-    cp.nearest_power_of_2()
-    cp.check_msb_set_unset()
+    # cp.nearest_power_of_2()
+    # cp.check_msb_set_unset()
+    string_chars_ops()
 
 
 if __name__ == '__main__':
---
+++ python/source/string_mysteries.py
@@ -0,0 +1,8 @@
+# mysteries of string handling in python
+
+def string_chars_ops():
+    s = "Gaurav Gupta"
+    print('length of the string s is :{}'.format(len(s)))
+    print('char at 0 is : {}'.format(s[0]))
+    print('last char is at location n - 1: {} which is {}'.format(len(s)-1, s[len(s) - 1]))
+
