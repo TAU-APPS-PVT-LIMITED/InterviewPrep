a49c3519691538bd748a39eae24ea2ddb519704a
Gaurav Gupta <1276964+ggthedev@users.noreply.github.com>
2024-06-20 12:40:39 +0530

    added line terminator example

--- python/sources/main.py
+++ python/sources/main.py
@@ -3,6 +3,8 @@ def bit_manipulations():
     mask = 0x1
     num = 44
     print(f"value of x:{format(bin(num))} & x - 1{format(bin(num - 1))}")
+    print("mask ", hex(mask), end=":")
+    print("is a crucial one with & operation")
 
 
 def main():
