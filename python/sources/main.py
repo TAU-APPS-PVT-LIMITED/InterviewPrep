9cd22af5690c5d7338af96e12c4c146deafc1dc6
Gaurav Gupta <1276964+ggthedev@users.noreply.github.com>
2024-06-20 12:19:08 +0530

    added simple file to kick of project

---
+++ python/sources/main.py
@@ -0,0 +1,13 @@
+
+def bit_manipulations():
+    mask = 0x1
+    num = 44
+    print(f"value of x:{format(bin(num))} & x - 1{format(bin(num - 1))}")
+
+
+def main():
+    bit_manipulations()
+
+
+if __name__ == '__main__':
+    main()
