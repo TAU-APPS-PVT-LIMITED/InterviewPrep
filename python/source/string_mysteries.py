568b6c23c037ad2bd02a120d4a8dcd593fedc528
Gaurav Gupta <1276964+ggthedev@users.noreply.github.com>
2024-06-21 08:35:47 +0530

    notes for reversing a string, slicing does not provide stream of characters, rather just one char at a time.

--- python/source/string_mysteries.py
+++ python/source/string_mysteries.py
@@ -5,4 +5,6 @@ def string_chars_ops():
     print('length of the string s is :{}'.format(len(s)))
     print('char at 0 is : {}'.format(s[0]))
     print('last char is at location n - 1: {} which is {}'.format(len(s)-1, s[len(s) - 1]))
-
+    print('To reverse a string, slicing does not work, hence loop is used')
+    for i in range(0, len(s)):
+        print(s[-(i+1)], end='')
