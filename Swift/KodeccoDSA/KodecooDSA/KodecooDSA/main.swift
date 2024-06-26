88fdd0b570ca2a845b7a2a5b5dcefc12579db375
Gaurav Gupta <1276964+ggthedev@users.noreply.github.com>
2024-06-26 21:13:47 +0530

    added new ops for linkedlist

--- Swift/KodeccoDSA/KodecooDSA/KodecooDSA/main.swift
+++ Swift/KodeccoDSA/KodecooDSA/KodecooDSA/main.swift
@@ -9,17 +9,18 @@ import Foundation
 
 //linked list init
 var ll = IntLL() //head and tail are nil
-
 ll.push(value: 4)
 print("Popping from ll: \(ll.pop()!)")
 ll.describe()
-
 ll.push(value: 5)
 ll.push(value: 6)
 ll.push(value: 7)
 ll.push(value: 8)
-
 ll.describe()
 print("Popping from ll: \(ll.pop()!)")
 ll.describe()
-
+ll.push(value: 9)
+ll.push(value: 10)
+ll.push(value: 11)
+ll.push(value: 12)
+ll.describe()
